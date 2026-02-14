5d05addeab85e68d89c71f1583714641be0248dc
StellrVR <dr.james.farrar@gmail.com>
2026-02-14 04:27:49 -0800

    add rss feed

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,8 @@
+const rssPlugin = require("@11ty/eleventy-plugin-rss");
+
 module.exports = function(eleventyConfig) {
+    eleventyConfig.addPlugin(rssPlugin);
+
     eleventyConfig.addPassthroughCopy({"styles.css": "styles.css"});
     eleventyConfig.addPassthroughCopy({"script.js": "script.js"});
     eleventyConfig.addPassthroughCopy({"assets": "assets"});
